fb9aed8a05322a75c9e2085e0b313a7fa172dbaa
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-12-19 17:53:39 +0000

    Repro

--- src/test/scala/foo/MySuite.scala
+++ src/test/scala/foo/MySuite.scala
@@ -1,12 +1,5 @@
 package foo
 
-import org.junit.runner.RunWith
-import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitRunner
-
-@RunWith(classOf[JUnitRunner])
-class MySuite extends FunSuite {
-  test("user-dir") {
-    println(s"Working directory: ${System.getProperty("user.dir")}")
-  }
+class MySuite {
+  val x =41
 }
