c071606016083a2534c96f65f2771e04e8f38868
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-07 19:35:53 +0000

    Automate reproduction

---
+++ reproduce.sh
@@ -0,0 +1,9 @@
+set -eux
+BRANCH=$1
+cat > instructions.txt <<EOF
+git clone -b $BRANCH https://github.com/olafurpg/intellij-bsp-pants
+cd intellij-bsp-pants
+./setup-bsp.sh
+EOF
+
+cat instructions.txt | pbcopy
