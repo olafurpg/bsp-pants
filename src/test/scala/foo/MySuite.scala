2f06402c1267df60327dcf1dfcefd246bd2472fd
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-28 17:45:11 +0000

    reproduce bloop shutdown

--- src/test/scala/foo/MySuite.scala
+++ src/test/scala/foo/MySuite.scala
@@ -2,9 +2,7 @@ package foo
 
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitRunner
 
-@RunWith(classOf[JUnitRunner])
 class MySuite extends FunSuite {
   test("auto-test works") {
     println("Auto-test!")
