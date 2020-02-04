7c5edaab873be1c007f05a88da489de3d89e6abd
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-02-04 11:35:34 +0000

    WIP

--- nested/inner/FooInner.java
+++ nested/inner/FooInner.java
@@ -1,7 +1,5 @@
 package nested.inner;
 
-import nested.inner.inner2.FooInner2;
-
 public class FooInner {
   @org.junit.Test public void testFooInner() {
     System.out.println("FOO" + new nested.testing.Helper());
