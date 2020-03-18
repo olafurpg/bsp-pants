2c6bd703fbf829d1dac96e43f96d2111bec83f78
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-03-18 14:29:24 +0000

    WIP

--- setup-bsp.sh
+++ setup-bsp.sh
@@ -1,1 +1,1 @@
-./coursier launch org.scalameta:metals_2.12:0.8.1+46-e5fa1ba9-SNAPSHOT -r sonatype:snapshots --main scala.meta.internal.pantsbuild.BloopPants -- create --intellij test-nop::
+./coursier launch org.scalameta:metals_2.12:0.8.1+46-e5fa1ba9-SNAPSHOT -r sonatype:snapshots --main scala.meta.internal.pantsbuild.BloopPants -- create --intellij scratchfile::
