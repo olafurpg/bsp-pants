a846bb11b90b80d569f2db034fb3ff7ff6edc0ed
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-04-15 12:38:18 +0000

    WIP

--- src/test/scala/bsp/MainSuite.scala
+++ src/test/scala/bsp/MainSuite.scala
@@ -1,6 +1,11 @@
 package bsp
 
+import org.junit.runner.RunWith
+import org.scalatestplus.junit.JUnitRunner
+
+//@RunWith(classOf[JUnitRunner])
 class MainSuite extends org.scalatest.FunSuite {
   test("bsp") {
+    println(new Main().greeting)
   }
 }
